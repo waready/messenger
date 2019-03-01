6886c8bdd9e0fc3e8ac086cf7cadba7f936ec128
waready <elbarto119@hotmail.com>
2019-02-28 20:42:59 -0500

    dasboar messenger persona

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<b-container>
+    <b-row>
+        <b-col cols="4">
+            <b-form-input  class=" my-1 text-center" type="text"
+            placeholder="Buscar contacto....">
+            </b-form-input>
+            <b-row class="p-2" align-h="center">
+                <b-col cols="12" md="3">
+                    <b-img rounded="circle" blank width= '60' height= '60' class= 'm1' blank-color="#777" alt="Center image" />
+                </b-col>
+                <b-col cols="6" align-self="center" class="d-none d-md-block">
+                    <p class="mb-1">Juan Ramos</p>
+                    <p class="text-muted small mb-1">Tu: hasta luego</p>
+                </b-col>
+                <b-col cols="3" class="d-none d-md-block">
+                    <p class="text-muted small">20:31 pm</p>
+                </b-col>
+            </b-row>
+           
+        </b-col>
+        <b-col cols="8"></b-col>
+    </b-row>
+</b-container>
 @endsection
